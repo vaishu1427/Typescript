3b848ee76a05bdfa96077c1c13ae5077c1f269e6
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2025-01-09 14:30:22 +0530

    Added basic types

---
+++ BasicsDemo/basicTypes.ts
@@ -0,0 +1,18 @@
+console.log("Hi let's finish...")
+
+// In typescript, both type assignment (Expicit) and type inference (Implicit) throws error for type mismatch. But not javascript.
+
+// let myvar = "string";
+// myvar = 2;
+
+// let myvar1: number = 9;
+// myvar1 = "Vaishu";
+
+const num = 5;
+const num1 = '10';
+console.log(num1+num)
+
+const num2:number = 5;
+const num3: number = 10;
+console.log(num2+num3)
+
