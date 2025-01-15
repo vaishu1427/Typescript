bd476f20c757a67acccf3b63717488526c5dddd9
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2025-01-15 22:28:05 +0530

    Added arrays type

---
+++ BasicsDemo/arrays.ts
@@ -0,0 +1,11 @@
+let arr1 = ['a','b','c'];
+
+let arr2: number[] = [1,2,3]
+
+let obj = {
+    hobbies:["run","swim","dance"]
+}
+
+for (let hobby of obj.hobbies){
+    console.log(hobby.toUpperCase())
+}
--- BasicsDemo/build/arrays.js
+++ BasicsDemo/build/arrays.js
@@ -1,1 +1,9 @@
-"use strict";
+var arr1 = ['a', 'b', 'c'];
+var arr2 = [1, 2, 3];
+var obj = {
+    hobbies: ["run", "swim", "dance"]
+};
+for (var _i = 0, _a = obj.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
